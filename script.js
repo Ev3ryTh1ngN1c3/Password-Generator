933a1016f36111a09e210b0bce381284318bacc6
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-06 14:05:07 -0500

    added code to js

--- script.js
+++ script.js
@@ -3,7 +3,30 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePasswordoptions() {
+  var length = parseInt(prompt("how many characters would you like your password to contain"), 10)
+  //if length is less than 8 and more than 128 
+  var hasspecialcharacters = confirm("click okay to confirm to include special characters")
+  var hasnumeralcharacters = confirm("click okay to confirm to include numerical characters")
+  var haslowercasecharacters = confirm("click okay to confirm to include lowercase characters")
+  var hasuppercasecharacters = confirm("click okay to confirm upper case characters")
 
+  if (
+    hasspecialcharacters === false && hasnumeralcharacters === false && haslowercasecharacters === false && hasuppercasecharacters === false) {
+    alert("must select at least one character type")
+    return null;
+  }
+  var passwordoptions = {
+    length: length,
+    hasspecialcharacters: hasspecialcharacters,
+    hasnumeralcharacters: hasnumeralcharacters,
+    haslowercasecharacters: haslowercasecharacters,
+    hasuppercasecharacters: hasuppercasecharacters
+
+  }
+  return passwordoptions;
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,21 +39,26 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var password=document.getElementById("password");
+var password = document.getElementById("password");
 
- function generatePassword() {
-    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-    var passwordLength = 8;
-    var password = "";
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  } 
-return password
- }
+function generatePassword() {
+  var options = generatePasswordoptions();
+  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  const uppercasecharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const lowercasecharacters = "abcdefghijklmnopqrstuvwxyz"
+  const numericalcharacters = "0123456789"
+  const specialcharacters = "!@#$%^&*()"
+  var passwordLength = 8;
+  var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  return password
+}
 
 function copyPassword() {
   var copyText = document.getElementById("password");
   copyText.select();
-  document.execCommand("copy");  
+  document.execCommand("copy");
 }
